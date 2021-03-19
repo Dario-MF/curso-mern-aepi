fd740daac1ae8b5fd20a1c7d2b188f077aec309d
Dario MF <ferrada_86@hotmail.com>
2021-03-19 22:41:52 +0100

    add variables apertura cierre server

--- Ejercicios/02-ejercicios/app-ej-02/middlewares/closeServer.js
+++ Ejercicios/02-ejercicios/app-ej-02/middlewares/closeServer.js
@@ -1,11 +1,13 @@
-
+require('dotenv').config();
 
 
 const closeServer = (req, res, next) => {
+    const cierre = process.env.HORA_CIERRE;
+    const apertura = process.env.HORA_APERTURA;
     const d = new Date();
     const hour = d.getHours();
 
-    if (hour >= 17 || hour <= 7) {
+    if (hour >= cierre || hour < apertura) {
         res.locals.error = 'No se puede acceder al servidor de: 17:00h ~ 08:00h';
         res.locals.status = 503;
         res.locals.message = 'Servicio no disponible'
